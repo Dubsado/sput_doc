9d7d519a225c72322edf75466a881108cbf0906b
Jake <jake@xyzdigital.com>
2023-03-10 08:07:41 -0800

    add alias for src files in editor

--- editor/vite.config.ts
+++ editor/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import * as path from 'path'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [react()],
+	resolve: {
+		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+	},
+	server: {
+		proxy: {
+			'/v1': {
+				target: 'http://0.0.0.0:3000',
+				changeOrigin: true,
+				secure: false
+			}
+		}
+	}
 })
